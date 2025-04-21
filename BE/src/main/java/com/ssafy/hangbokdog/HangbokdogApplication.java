a378969aa812d62eb1d91df22b8de95b616407b2
박종하 <jonghaparkdevelop@gmail.com>
2025-04-21 17:55:11 +0900

    [S12P31A103-129] feature: Initial Commit

---
+++ BE/src/main/java/com/ssafy/hangbokdog/HangbokdogApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.hangbokdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangbokdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangbokdogApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/ssafy/hangbokdog/HangbokdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.hangbokdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangbokdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
