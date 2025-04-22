a549fd757ec95329c62f71736b37563c0947d0d8
jinsu868 <mjinsu098@gmail.com>
2025-04-22 11:09:23 +0900

    [S12P31A103-123] feature : define Member domain

---
+++ BE/src/main/java/com/ssafy/hangbokdog/member/domain/Member.java
@@ -0,0 +1,62 @@
+package com.ssafy.hangbokdog.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nick_name", nullable = false, length = 64)
+    private String nickName;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "mileage", nullable = false)
+    private int mileage;
+
+    @Column(name = "social_id", nullable = false, length = 32)
+    private String socialId;
+
+    @Enumerated(EnumType.STRING)
+    private Grade grade;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    @Column(name = "description", length = 1024, nullable = false)
+    private String description;
+
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted;
+
+    @Column(name = "dark_mode_check", nullable = false)
+    private boolean darkModeCheck;
+
+    @Column(name = "emergency_notification_check", nullable = false)
+    private boolean emergencyNotificationCheck;
+
+    @Column(name = "fcm_token")
+    private String fcmToken;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "profile_image", nullable = false)
+    private String profileImage;
+}
