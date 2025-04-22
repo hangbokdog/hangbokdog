b8c6c4640eb58aae9b05c313b27ec166c91c3523
jinsu868 <mjinsu098@gmail.com>
2025-04-22 12:16:16 +0900

    [S12P31A103-145] fix : change member id column to member_id

--- BE/src/main/java/com/ssafy/hangbokdog/member/domain/Member.java
+++ BE/src/main/java/com/ssafy/hangbokdog/member/domain/Member.java
@@ -19,6 +19,7 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
     @Column(name = "nick_name", nullable = false, length = 64)
