5a76d4f2141113e5302f5150602ee71cc5f49817
jinsu868 <mjinsu098@gmail.com>
2025-04-22 13:02:51 +0900

    [S12P31A103-153] feature : define custom error code class

---
+++ BE/src/main/java/com/ssafy/hangbokdog/common/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.ssafy.hangbokdog.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    INVALID_REQUEST(1000, "유효하지 않은 요청입니다."),
+
+    UNABLE_TO_GET_USER_INFO(2001, "소셜 로그인 공급자로부터 유저 정보를 받아올 수 없습니다."),
+    UNABLE_TO_GET_ACCESS_TOKEN(2002, "소셜 로그인 공급자로부터 인증 토큰을 받아올 수 없습니다."),
+
+    UNAUTHORIZED_ACCESS(3000, "접근할 수 없는 리소스입니다."),
+    INVALID_REFRESH_TOKEN(3001, "유효하지 않은 Refresh Token입니다."),
+    FAILED_TO_VALIDATE_TOKEN(3002, "토큰 검증에 실패했습니다."),
+    INVALID_ACCESS_TOKEN(3003, "유효하지 않은 Access Token입니다."),
+
+    VALIDATION_FAIL(4000, "유효하지 않은 형식입니다."),
+    INTERNAL_SERVER_ERROR(4001, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+}
+
