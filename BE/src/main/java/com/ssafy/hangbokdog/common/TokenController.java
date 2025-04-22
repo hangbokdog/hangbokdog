613a956ae2dd930fc91075a307f43af4455274d0
jinsu868 <mjinsu098@gmail.com>
2025-04-22 15:32:50 +0900

    [S12P31A103-99] fix : resolve checkStyle

--- BE/src/main/java/com/ssafy/hangbokdog/common/TokenController.java
+++ BE/src/main/java/com/ssafy/hangbokdog/common/TokenController.java
@@ -1,13 +1,15 @@
 package com.ssafy.hangbokdog.common;
 
-import com.ssafy.hangbokdog.auth.domain.AuthTokens;
-import com.ssafy.hangbokdog.auth.util.JwtUtils;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.hangbokdog.auth.domain.AuthTokens;
+import com.ssafy.hangbokdog.auth.util.JwtUtils;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
--- BE/src/main/java/com/ssafy/hangbokdog/common/config/WebConfig.java
+++ BE/src/main/java/com/ssafy/hangbokdog/common/config/WebConfig.java
@@ -1,12 +1,13 @@
 package com.ssafy.hangbokdog.common.config;
 
-import com.ssafy.hangbokdog.auth.resolver.AuthUserArgumentResolver;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ssafy.hangbokdog.auth.resolver.AuthUserArgumentResolver;
 
 import lombok.RequiredArgsConstructor;
 
