8777cd221bb383804c14c58ed4d2c881ed2e59d7
박종하 <jonghaparkdevelop@gmail.com>
2025-04-22 14:31:42 +0900

    [S12P31A103-181] feature: Define Center domain

---
+++ BE/src/main/java/com/ssafy/hangbokdog/center/domain/Center.java
@@ -0,0 +1,36 @@
+package com.ssafy.hangbokdog.center.domain;
+
+import com.ssafy.hangbokdog.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Center extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "center_id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	public static Center create(String name) {
+		return new Center(name);
+	}
+
+	private Center(
+		String name
+	) {
+		this.name = name;
+	}
+}
