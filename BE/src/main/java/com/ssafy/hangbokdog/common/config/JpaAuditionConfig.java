b62458e15cd8ff3ef3fa814c2be72ab5d868fc30
박종하 <jonghaparkdevelop@gmail.com>
2025-04-22 14:28:34 +0900

    [S12P31A103-183] chore: Add JpaAuditionConfig

---
+++ BE/src/main/java/com/ssafy/hangbokdog/common/config/JpaAuditionConfig.java
@@ -0,0 +1,9 @@
+package com.ssafy.hangbokdog.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditionConfig {
+}
