f9d6d1d607d61dd0eca933ad0201536b3dc91c65
jinsu868 <mjinsu098@gmail.com>
2025-04-22 15:30:29 +0900

    [S12P31A103-99] : feature : implement test token issuing

---
+++ BE/src/main/java/com/ssafy/hangbokdog/common/TokenController.java
@@ -0,0 +1,23 @@
+package com.ssafy.hangbokdog.common;
+
+import com.ssafy.hangbokdog.auth.domain.AuthTokens;
+import com.ssafy.hangbokdog.auth.util.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/test-tokens")
+public class TokenController {
+
+    private final JwtUtils jwtUtils;
+
+    @GetMapping
+    public ResponseEntity<AuthTokens> issueTestTokens(@RequestParam(name = "id") Long id) {
+        return ResponseEntity.ok(jwtUtils.createLoginToken(String.valueOf(id)));
+    }
+}
