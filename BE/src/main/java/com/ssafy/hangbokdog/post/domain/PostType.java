b05d77af97a5fbbee7fbd21f21bd2ff9e09e6dbc
엄예림 <djadpfa1002@naver.com>
2025-04-22 12:37:58 +0900

    [S12P31A103-137] feature: Defined Post & PostType Domain

---
+++ BE/src/main/java/com/ssafy/hangbokdog/post/domain/PostType.java
@@ -0,0 +1,27 @@
+package com.ssafy.hangbokdog.post.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import com.ssafy.hangbokdog.common.entity.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostType extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postTypeId;
+
+    @Column(nullable = false, length = 50)
+    private String postTypeName;
+
+}
