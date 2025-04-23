4567cd7469d4b852a3079282523d4c856cbbc96b
박종하 <jonghaparkdevelop@gmail.com>
2025-04-23 13:06:45 +0900

    [S12P31A103-214] refactor: Check Member Grade of CenterController

--- BE/src/main/java/com/ssafy/hangbokdog/center/presentation/CenterController.java
+++ BE/src/main/java/com/ssafy/hangbokdog/center/presentation/CenterController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.hangbokdog.auth.annotation.AdminMember;
 import com.ssafy.hangbokdog.center.application.CenterService;
 import com.ssafy.hangbokdog.center.dto.request.CenterCreateRequest;
+import com.ssafy.hangbokdog.member.domain.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,8 +23,9 @@ public class CenterController {
 	private final CenterService centerService;
 
 	@PostMapping
-	public ResponseEntity<Void> addCenter(@RequestBody CenterCreateRequest request) {
-
+	public ResponseEntity<Void> addCenter(
+		@AdminMember Member member,
+		@RequestBody CenterCreateRequest request) {
 		Long centerId = centerService.createCenter(request);
 		return ResponseEntity.created(URI.create("/api/v1/centers/" + centerId))
 			.build();
