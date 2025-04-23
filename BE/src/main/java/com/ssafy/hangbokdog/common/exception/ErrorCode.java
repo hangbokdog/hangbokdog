764a26a3475ee607c47a40b809fea4da602078ea
jinsu868 <mjinsu098@gmail.com>
2025-04-23 16:00:24 +0900

    [S12P31A103-165] : add custom error code

--- BE/src/main/java/com/ssafy/hangbokdog/common/exception/ErrorCode.java
+++ BE/src/main/java/com/ssafy/hangbokdog/common/exception/ErrorCode.java
@@ -28,7 +28,8 @@ public enum ErrorCode {
     DOG_NOT_FOUND(6000, "존재하지 않는 강아지입니다."),
 
     PRODUCT_NOT_FOUND(7000, "존재하지 않는 상품입니다."),
-    PRODUCT_NOT_ON_SALE(7001, "현재 판매중인 상품이 아닙니다.");
+    PRODUCT_NOT_ON_SALE(7001, "현재 판매중인 상품이 아닙니다."),
+    CANNOT_PURCHASE_OWN_PRODUCT(7002, "내 상품은 구매할 수 없습니다.");
 
     private final int code;
     private final String message;
