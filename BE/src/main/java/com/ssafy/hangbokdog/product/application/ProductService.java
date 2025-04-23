6c4376e0c7782c6d500c8423042a3ad1f3638bc6
jinsu868 <mjinsu098@gmail.com>
2025-04-23 11:13:17 +0900

    [S12P31A103-163] feature : implement product update

--- BE/src/main/java/com/ssafy/hangbokdog/product/application/ProductService.java
+++ BE/src/main/java/com/ssafy/hangbokdog/product/application/ProductService.java
@@ -1,14 +1,21 @@
 package com.ssafy.hangbokdog.product.application;
 
+import static com.ssafy.hangbokdog.common.exception.ErrorCode.NOT_SELLER;
+import static com.ssafy.hangbokdog.common.exception.ErrorCode.PRODUCT_NOT_FOUND;
+import static com.ssafy.hangbokdog.common.exception.ErrorCode.PRODUCT_NOT_ON_SALE;
+
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.hangbokdog.common.exception.BadRequestException;
 import com.ssafy.hangbokdog.common.model.PageInfo;
 import com.ssafy.hangbokdog.member.domain.Member;
 import com.ssafy.hangbokdog.product.domain.Product;
 import com.ssafy.hangbokdog.product.domain.repository.ProductRepository;
 import com.ssafy.hangbokdog.product.dto.request.ProductCreateRequest;
+import com.ssafy.hangbokdog.product.dto.request.ProductUpdateRequest;
 import com.ssafy.hangbokdog.product.dto.response.ProductResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -40,5 +47,31 @@ public class ProductService {
             String pageToken
     ) {
         return productRepository.findAll(pageToken);
+    }
+
+    @Transactional
+    public void update(
+            Long productId,
+            Member member,
+            ProductUpdateRequest productUpdateRequest,
+            List<String> imageUrls
+    ) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BadRequestException(PRODUCT_NOT_FOUND));
+
+        if (!product.isSeller(member)) {
+            throw new BadRequestException(NOT_SELLER);
+        }
+
+        if (!product.isOnSale()) {
+            throw new BadRequestException(PRODUCT_NOT_ON_SALE);
+        }
+
+        product.update(
+                productUpdateRequest.name(),
+                productUpdateRequest.description(),
+                productUpdateRequest.price(),
+                imageUrls
+        );
     }
 }
