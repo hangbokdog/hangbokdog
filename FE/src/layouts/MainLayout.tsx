9a9f91420e13193ff0faeb8bc1eec269e57e672d
이재백 <dbswlrla112@naver.com>
2025-04-25 17:15:38 +0900

    [S12P31A103-328] feature: Implement Header Component

--- FE/src/layouts/MainLayout.tsx
+++ FE/src/layouts/MainLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "@/components/common/Header";
 export default function MainLayout() {
 	return (
 		<div className="w-screen h-screen flex justify-center">
 			<div className="flex flex-col h-full min-w-[402px] bg-[#F6F7F9]">
+				<Header />
 				<Outlet />
 			</div>
 		</div>
