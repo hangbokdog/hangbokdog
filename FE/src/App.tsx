f89dacfa3f18f9d960e53d7e4726af600df445b1
dbswlrla112@naver.com <dbswlrla112@naver.com>
2025-04-26 21:10:44 +0900

    [S12P31A103-336] feature: Fix styles

--- FE/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-	return (
-		<main className="flex-1 overflow-y-auto mb-14">
-			<div>app</div>
-		</main>
-	);
-}
--- FE/src/layouts/MainLayout.tsx
+++ FE/src/layouts/MainLayout.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
-import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
 	return (
 		<div className="w-screen h-screen flex justify-center">
-			<div className="flex flex-col h-full min-w-[402px] bg-[#F6F7F9]">
+			<div className="flex flex-col h-full min-w-[402px] bg-background shadow-custom-xs">
 				<Header />
-				<Outlet />
+				<main className="flex-1 overflow-y-auto mb-14 px-2.5 pt-2.5 scrollbar-hidden">
+					<Outlet />
+				</main>
 			</div>
 			<Footer />
 		</div>
