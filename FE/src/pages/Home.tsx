04757c48f8a7fe71eaa7b978596b544f1f6ed62b
dbswlrla112@naver.com <dbswlrla112@naver.com>
2025-04-27 17:03:17 +0900

    [S12P31A103-339] feature: Implement General Main Page Layout Component

--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -1,3 +1,19 @@
+import DogPanel from "@/components/home/DogPanel";
+import DonationPanel from "@/components/home/DonationPanel";
+import EmergencyPanel from "@/components/home/EmergencyPanel";
+import MyDonationPanel from "@/components/home/MyDonationPanel";
+import VolunteerPanel from "@/components/home/VolunteerPanel";
+
 export default function Home() {
-	return <div>app</div>;
+	return (
+		<div className="flex flex-col gap-3 w-full">
+			<EmergencyPanel />
+			<VolunteerPanel />
+			<DogPanel />
+			<div className="max-w-[382px] grid grid-cols-2 gap-3">
+				<DonationPanel />
+				<MyDonationPanel />
+			</div>
+		</div>
+	);
 }
