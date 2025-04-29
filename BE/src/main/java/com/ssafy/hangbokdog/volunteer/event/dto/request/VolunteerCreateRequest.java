c4480eef9fae7e63c56294ea9b4ee2eb06631cd7
엄예림 <djadpfa1002@naver.com>
2025-04-29 10:55:21 +0900

    [S12P31A103-352] refactor: extracted SlotDto

--- BE/src/main/java/com/ssafy/hangbokdog/volunteer/event/dto/request/VolunteerCreateRequest.java
+++ BE/src/main/java/com/ssafy/hangbokdog/volunteer/event/dto/request/VolunteerCreateRequest.java
@@ -1,11 +1,10 @@
 package com.ssafy.hangbokdog.volunteer.event.dto.request;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 import com.ssafy.hangbokdog.volunteer.event.domain.LocationType;
-import com.ssafy.hangbokdog.volunteer.event.domain.SlotType;
+import com.ssafy.hangbokdog.volunteer.event.dto.SlotDto;
 
 // TODO: 필요 시, 활동 일지 API 분리
 public record VolunteerCreateRequest(
@@ -18,14 +17,6 @@ public record VolunteerCreateRequest(
         String activityLog,
         String precaution,
         String info,
-        List<SlotRequest> slots
+        List<SlotDto> slots
 ) {
-
-    public record SlotRequest(
-            SlotType slotType,
-            LocalTime startTime,
-            LocalTime endTime,
-            int capacity
-    ) {
-    }
 }
