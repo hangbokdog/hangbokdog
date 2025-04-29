505c3e743cd9dfc6356a1e411ffbcfdb9159c501
최준혁 <raonrabbit@gmail.com>
2025-04-29 13:07:13 +0900

    [S12P31A103-430] chore: Update admin emergency panel structure according to requirements

--- FE/src/components/common/emergency/movingListItem.tsx
+++ FE/src/components/common/emergency/movingListItem.tsx
@@ -1,35 +1,43 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaPaw } from "react-icons/fa";
 import { MdSend } from "react-icons/md";
 
 interface MovingListItemProps {
-    name: string;
-    startLocation: string;
-    endLocation: string;
-    date: string;
-    index: number;
+	img: string;
+	name: string;
+	startLocation: string;
+	endLocation: string;
+	date: string;
+	index: number;
 }
 
 export default function MovingListItem(props: MovingListItemProps) {
-    const { name, startLocation, endLocation, date, index } = props;
+	const { img, name, startLocation, endLocation, date, index } = props;
 
-    return (
-        <div className={`flex rounded-full ${ index % 2 === 1 && "bg-background"} text-grayText text-base font-medium items-center px-5 py-1`}>
-            <div className="w-8 text-[var(--color-blueGray)] font-semibold">
-                {name}
-            </div>
-            <div className="flex-1/2 flex justify-center items-center gap-2.5">
-                <div className="flex-1/3 text-center">
-                    {startLocation}
-                </div>
-                <div className="flex-1">
-                    <MdSend className="text-[var(--color-lightGray)]" />
-                </div>
-                <div className="flex-1/3 text-center">
-                    {endLocation}
-                </div>
-            </div>
-            <div className="w-16 font-light text-[var(--color-blueGray)] text-start">
-                {date}
-            </div>
-        </div>
-    )
+	return (
+		<div
+			className={`flex rounded-full ${index % 2 === 1 && "bg-background"} text-grayText text-base font-medium items-center px-5 py-1`}
+		>
+			<div className="flex-1 flex items-center gap-2 font-semibold">
+				<Avatar className="w-6 h-6 flex justify-center items-center">
+					<AvatarImage src={img} />
+					<AvatarFallback className="text-center bg-superLightGray text-grayText">
+						{name[0]}
+					</AvatarFallback>
+				</Avatar>
+				{name}
+				<FaPaw />
+			</div>
+			<div className="flex-1/4 flex justify-center items-center gap-2.5">
+				<div className="flex-1/3 text-center">{startLocation}</div>
+				<div className="flex-1">
+					<MdSend className="text-[var(--color-lightGray)]" />
+				</div>
+				<div className="flex-1/3 text-center">{endLocation}</div>
+			</div>
+			<div className="w-16 font-light text-[var(--color-blueGray)] text-start">
+				{date}
+			</div>
+		</div>
+	);
 }
