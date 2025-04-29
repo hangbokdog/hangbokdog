54d1e43be4c45919fbd2d296cbd6c80096d6d073
최준혁 <raonrabbit@gmail.com>
2025-04-29 09:19:18 +0900

    [S12P31A103-405] feature: Implement admin main page component

---
+++ FE/src/pages/manager/ManagerMain.tsx
@@ -0,0 +1,13 @@
+import ManagerEmergencyPanel from "@/components/manager/main/ManagerEmergencyPanel";
+import ManagerManagePanel from "@/components/manager/main/ManagerManagePanel";
+import ManagerRequestsPanel from "@/components/manager/main/ManagerRequestsPanel";
+
+export default function ManagerMain() {
+	return (
+		<div className="flex flex-col gap-3">
+			<ManagerEmergencyPanel />
+			<ManagerRequestsPanel />
+			<ManagerManagePanel />
+		</div>
+	);
+}
