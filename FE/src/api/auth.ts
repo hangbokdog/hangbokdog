c0946c517cd90deb52ccf133639b23d1183c7905
이재백 <dbswlrla112@naver.com>
2025-04-30 15:02:27 +0900

    [S12P31A103-516] feature: Implement navar login api

---
+++ FE/src/api/auth.ts
@@ -0,0 +1,19 @@
+import type { OauthLoginResponse } from "@/types/auth";
+import localAxios from "./http-commons";
+
+export const NaverLoginAPI = async ({
+	code,
+	state,
+}: {
+	code: string;
+	state: string;
+}): Promise<OauthLoginResponse> => {
+	const response = await localAxios.post<OauthLoginResponse>(
+		"auth/login/naver",
+		{
+			code,
+			state,
+		},
+	);
+	return response.data;
+};
