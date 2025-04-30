2e98c1106b1e4018430faad37e0132b2e5b7c541
이재백 <dbswlrla112@naver.com>
2025-04-30 14:49:04 +0900

    [S12P31A103-509] feature: Implement SignUpButton Component

---
+++ FE/src/components/signup/SignUpButton.tsx
@@ -0,0 +1,10 @@
+export default function SignUpButton() {
+	return (
+		<button
+			type="button"
+			className="w-full rounded-[8px] bg-main inline-flex items-center justify-center gap-2 py-4"
+		>
+			<span className="text-white font-semibold text-lg">가입하기</span>
+		</button>
+	);
+}
