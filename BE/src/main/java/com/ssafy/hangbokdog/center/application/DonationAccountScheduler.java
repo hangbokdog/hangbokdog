bfd7bc0e86011a878dbbc8a69aecb85ce6f800b7
박종하 <jonghaparkdevelop@gmail.com>
2025-04-30 10:53:36 +0900

    [S12P31A103-482] style: Update classes to style code

--- BE/src/main/java/com/ssafy/hangbokdog/center/application/DonationAccountScheduler.java
+++ BE/src/main/java/com/ssafy/hangbokdog/center/application/DonationAccountScheduler.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Component;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DonationAccountScheduler {
@@ -18,7 +16,6 @@ public class DonationAccountScheduler {
 	@Scheduled(fixedRate = 300000)
 	@SchedulerLock(name = "donationAccountScheduler", lockAtLeastFor = "290s", lockAtMostFor = "299s")
 	public void applyTransactionsToDonationAccount() {
-		log.info("Applying transactions to DonationAccount");
 		donationAccountService.applyTransactionsToDonationAccount();
 	}
 
