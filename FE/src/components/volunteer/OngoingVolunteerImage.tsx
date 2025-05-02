5904af3a73c92c2eb59c66bbe2463b0477fd6ee0
이재백 <dbswlrla112@naver.com>
2025-05-02 16:35:01 +0900

    [S12P31A103-617] feature: Implement OngoingVolunteerImage component

---
+++ FE/src/components/volunteer/OngoingVolunteerImage.tsx
@@ -0,0 +1,21 @@
+import { IoIosArrowForward } from "react-icons/io";
+
+interface OngoingVolunteerImageProps {
+	src: string;
+	alt: string;
+}
+
+export default function OngoingVolunteerImage({
+	src,
+	alt,
+}: OngoingVolunteerImageProps) {
+	return (
+		<div className="relative">
+			<img className="object-cover h-full" src={src} alt={alt} />
+			<div className="absolute inset-0 bg-custom-gradient" />
+			<span className="absolute bg-white right-1 bottom-1 rounded-full inline-flex items-center justify-center px-2 py-1">
+				<IoIosArrowForward className="size-4 text-grayText" />
+			</span>
+		</div>
+	);
+}
