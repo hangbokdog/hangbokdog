4ce00efe66153ac82d5d33983fe1a9dcfa31d5d9
이재백 <dbswlrla112@naver.com>
2025-05-02 12:41:57 +0900

    [S12P31A103-576] feature: Implement adopter prerequisites content component

---
+++ FE/src/components/adoption/AdopterRequiredContent.tsx
@@ -0,0 +1,28 @@
+export default function AdopterRequiredContent() {
+	return (
+		<div className="flex flex-col gap-2 p-4 bg-white rounded-[8px]">
+			<span>1. 주신청자가 27세이상이어야 합니다.</span>
+			<span>
+				2. 경제력있는 분이셔야할것(학생X, 대학생X, 미성년자X), 단
+				한가정의 주부는 가능합니다.
+			</span>
+			<span>3. 가족동의여부확인(혼자거주시에도 확인필수)</span>
+			<span>
+				4. 입양견은 모두 실내생활 필수(크면 마당, 접종 종료후 마당 모두
+				불가)
+			</span>
+			<span>5. 중문 또는 안전문 설치 필수, 중성화, 내장칩동의 필수</span>
+			<span>
+				6. 미혼, 신혼부부, 어린자녀가있는 가정은 상담이 더까다롭습니다.
+			</span>
+			<span>
+				7. 40세미만 신혼 부부는 양가(친정, 시댁) 모두 상담하오니 연락처
+				꼭 기재바랍니다.
+			</span>
+			<span>
+				8. 실외에서는 목줄&하네스와 2중리쉬 필수, 트인 공간에서 오프리쉬
+				불가
+			</span>
+		</div>
+	);
+}
