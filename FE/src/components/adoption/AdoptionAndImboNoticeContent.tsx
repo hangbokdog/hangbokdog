b2b81a762172c252ffdc5321a60ae4a819fa3738
이재백 <dbswlrla112@naver.com>
2025-05-02 12:42:03 +0900

    [S12P31A103-577] feature: Implement pre-adoption notice content component

---
+++ FE/src/components/adoption/AdoptionAndImboNoticeContent.tsx
@@ -0,0 +1,41 @@
+export default function AdoptionAndImboNoticeContent() {
+	return (
+		<div className="flex flex-col gap-2 p-4 bg-white rounded-[8px]">
+			<span>
+				1. 행동하는 동물사랑 입양 및 임시보호 신청서를 받은 후{" "}
+				<span className="font-bold underline">
+					첫 상담까지 약 14일 정도 기간이 소요됩니다.
+				</span>
+			</span>
+			<span>
+				2. 단체 생활로 인해 신청하신 동물의 보호소, 위탁처 등에서와
+				가정에서의 생활 모습이 다를 수 있습니다.
+			</span>
+			<span>
+				3. 입양 및 임시보호 신청서를 작성하여 제출한다고 하여 확정
+				승인이 보장되지는 않습니다.
+			</span>
+			<span>
+				4. 동일 견에 대해 여러 신청서가 들어올 경우 입양이 우선 진행,
+				가장 적합한 환경으로 진행됩니다.
+			</span>
+			<span className="font-bold">
+				5. 입양시 입양하시는 멍이에 대해서 구조 후 진행된 최초 1회
+				진료(중성화 접종 내외부 사상충검사)에 대해 후원금 기부
+				부탁드립니다(기부금영수증 발급가능)
+			</span>
+			<span>
+				6. 12세미만 아동이 있으실 경우 상담이
+				제한되실수있습니다.(임시보호,입양 모두)
+			</span>
+			<span className="font-bold">
+				7. 서울,경기 수도권 외 지역으로는 입양만 진행가능합니다.(신청
+				강아지에 따라 반려될수도있음)
+			</span>
+			<span>
+				8. 다견다묘가정의 경우 합이 3마리 이상일때 임보입양이
+				반려됩니다.
+			</span>
+		</div>
+	);
+}
