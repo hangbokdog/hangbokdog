227a4acdbd84becf08290101a787ca9c0af49ae6
이재백 <dbswlrla112@naver.com>
2025-05-06 21:29:02 +0900

    [S12P31A103-669] feature: Add accessToken to headers after OAuth login

--- FE/src/api/http-commons.ts
+++ FE/src/api/http-commons.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import useAuthStore from "@/lib/store/authStore";
 
 const localAxios = axios.create({
 	baseURL: `${import.meta.env.VITE_API_URI}`,
@@ -7,5 +8,24 @@ const localAxios = axios.create({
 		"Content-Type": "application/json;charset=utf-8",
 	},
 });
+
+localAxios.interceptors.request.use(
+	(config) => {
+		let token = useAuthStore.getState().accessToken;
+
+		if (!token) {
+			token = sessionStorage.getItem("accessToken");
+		}
+
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
+);
 
 export default localAxios;
