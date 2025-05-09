cb1866a119b410a899615a3853a228d4b8d68849
최준혁 <raonrabbit@gmail.com>
2025-05-09 17:13:12 +0900

    [S12P31A103-822] feature: Connect API for dog detail and medical records

--- FE/src/api/dog.ts
+++ FE/src/api/dog.ts
@@ -1,4 +1,10 @@
-import type { DogLatestResponse } from "@/types/dog";
+import type {
+	DogBreed,
+	DogLatestResponse,
+	DogStatus,
+	Gender,
+	MedicalType,
+} from "@/types/dog";
 import localAxios from "./http-commons";
 
 export const createDogAPI = async (data: FormData) => {
@@ -7,7 +13,7 @@ export const createDogAPI = async (data: FormData) => {
 			"Content-Type": "multipart/form-data",
 		},
 	});
-	return response.data;
+	return response.data.dogId;
 };
 
 export const getLatestDogAPI = async (
@@ -17,4 +23,60 @@ export const getLatestDogAPI = async (
 		params: { centerId },
 	});
 	return response.data;
+};
+
+export interface DogDetailResponse {
+	dogId: number;
+	dogStatus: DogStatus;
+	centerId: number;
+	centerName: string;
+	dogName: string;
+	profileImageUrl: string;
+	color: string[];
+	rescuedDate: string;
+	weight: number;
+	description: string;
+	isStar: boolean;
+	gender: Gender;
+	isNeutered: boolean;
+	breed: DogBreed;
+	age: number;
+	location: string;
+	isLiked: boolean;
+	favoriteCount: number;
+	currentSponsorCount: number;
+}
+
+export const fetchDogDetail = async (dogId: number, centerId: string) => {
+	const response = await localAxios.get(`/dogs/${dogId}`, {
+		params: { centerId },
+	});
+	const data = response.data as DogDetailResponse;
+	return data;
+};
+
+export interface DogSponsor {
+	memberId: number;
+	name: string;
+	profileImage: string;
+	startTime: string;
+}
+
+export const fetchDogSponsors = async (dogId: number) => {
+	const response = await localAxios.get(`/dogs/fosters/${dogId}`);
+	return response.data as DogSponsor[];
+};
+
+export interface MedicalHistoryResponse {
+	id: number;
+	content: string;
+	medicalPeriod: number;
+	medicalType: MedicalType;
+	operatedDate: string;
+	image: string;
+}
+
+export const fetchDogMedicalHistory = async (dogId: number) => {
+	const response = await localAxios.get(`/dogs/${dogId}/medical-history`);
+	return response.data as MedicalHistoryResponse[];
 };
