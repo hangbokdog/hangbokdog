17b6a32eb26710eb05a198c35eb742ea132c9767
이재백 <dbswlrla112@naver.com>
2025-05-09 16:43:47 +0900

    [S12P31A103-809] feature: Add subtitle prop to PanelTitle component

--- FE/src/components/common/PanelTitle.tsx
+++ FE/src/components/common/PanelTitle.tsx
@@ -3,12 +3,18 @@ import { Link } from "react-router-dom";
 interface PanelTitleProps {
 	title: string;
 	link: string;
+	subTitle?: string;
 }
 
-export default function PanelTitle({ title, link }: PanelTitleProps) {
+export default function PanelTitle({ title, link, subTitle }: PanelTitleProps) {
 	return (
 		<div className="flex p-2.5 justify-between items-center text-grayText">
-			<span className="font-bold text-lg">{title}</span>
+			<div className="flex items-end gap-2.5 text-lg">
+				<span className="font-bold text-lg">{title}</span>
+				{subTitle && (
+					<span className="text-blue font-semibold">{subTitle}</span>
+				)}
+			</div>
 			<Link to={link} className="text-xs border-b-1 border-b-grayText">
 				더보기
 			</Link>
