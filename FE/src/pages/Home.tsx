d757db87e5c1bd1f37c7f3dedf84a81ecf3d5027
이재백 <dbswlrla112@naver.com>
2025-05-09 16:44:13 +0900

    [S12P31A103-812] feature: Update Home according to latest puppy fetch logic

--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { getLatestDogAPI } from "@/api/dog";
 import { getLatestVolunteerAPI } from "@/api/volunteer";
 import EmptyState from "@/components/common/EmptyState";
+import { DogPanelSkeleton } from "@/components/dog/SkeletonComponents";
 import DogPanel from "@/components/home/DogPanel";
 import DonationPanel from "@/components/home/DonationPanel";
 import EmergencyPanel from "@/components/home/EmergencyPanel";
@@ -13,14 +15,21 @@ export default function Home() {
 	const centerId = useCenterStore.getState().selectedCenter?.centerId;
 
 	const { data: volunteers, isLoading: isVolunteersLoading } = useQuery({
-		queryKey: ["volunteers", centerId],
+		queryKey: ["volunteers", "latest", centerId],
 		queryFn: () => getLatestVolunteerAPI({ centerId: centerId as string }),
+		enabled: !!centerId,
+	});
+
+	const { data: dogs, isLoading: isDogsLoading } = useQuery({
+		queryKey: ["dogs", "latest", centerId],
+		queryFn: () => getLatestDogAPI(centerId as string),
 		enabled: !!centerId,
 	});
 
 	return (
 		<div className="flex flex-col gap-3 w-full">
 			<EmergencyPanel />
+
 			{isVolunteersLoading ? (
 				<VolunteerPanelSkeleton />
 			) : volunteers && volunteers.length > 0 ? (
@@ -33,7 +42,20 @@ export default function Home() {
 					<EmptyState message="자원봉사 일정이 없습니다." />
 				</div>
 			)}
-			<DogPanel />
+
+			{isDogsLoading ? (
+				<DogPanelSkeleton />
+			) : dogs && dogs.dogSummaries.length > 0 ? (
+				<DogPanel count={dogs.count} dogSummaries={dogs.dogSummaries} />
+			) : (
+				<div className="flex flex-col">
+					<span className="text-xl font-bold mb-2.5">
+						보호중인 아이들
+					</span>
+					<EmptyState message="보호중인 아이들이 없습니다." />
+				</div>
+			)}
+
 			<div className="max-w-[420px] mx-2.5 grid grid-cols-2 gap-3 pb-2.5">
 				<DonationPanel />
 				<MyDonationPanel />
