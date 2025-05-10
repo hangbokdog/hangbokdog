cf938036b3c2d86501b8b465ea629c0784ad7db9
freeftr <jonghaparkdevelop@gmail.com>
2025-05-10 16:03:08 +0900

    [S12P31A103-840] refactor: Change getMyCenters response spec

--- BE/src/main/java/com/ssafy/hangbokdog/center/dto/response/MyCenterResponse.java
+++ BE/src/main/java/com/ssafy/hangbokdog/center/dto/response/MyCenterResponse.java
@@ -2,12 +2,14 @@ package com.ssafy.hangbokdog.center.dto.response;
 
 import java.time.LocalDateTime;
 
+import com.ssafy.hangbokdog.center.domain.enums.CenterCity;
 import com.ssafy.hangbokdog.center.domain.enums.CenterGrade;
 
 public record MyCenterResponse(
 	Long centerId,
 	String centerName,
 	CenterGrade centerGrade,
+	CenterCity centerCity,
 	LocalDateTime joinedDate
 ) {
 }
