49c501d6ba0d9b857a3dc2b67f3b19b3394c6b39
이재백 <dbswlrla112@naver.com>
2025-05-11 01:15:12 +0900

    [S12P31A103-864] feature: Update styles in DogInfoItem

--- FE/src/components/dog/DogInfoItem.tsx
+++ FE/src/components/dog/DogInfoItem.tsx
@@ -12,7 +12,7 @@ export default function DogInfoItem({
 	return (
 		<div className={`flex  ${className}`}>
 			<span className="flex-1/5 font-medium py-2 px-2 w-20">{label}</span>
-			<span className="flex-4/5 text-grayText font-semibold bg-background py-2 px-5 rounded-full">
+			<span className="flex-4/5 text-grayText font-semibold bg-background py-2 px-5 rounded-[8px]">
 				{value}
 			</span>
 		</div>
