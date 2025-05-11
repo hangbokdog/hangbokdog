f2f9ca88ad1d5d862eea46ed980c2fe33e34a4f7
raonrabbit <raonrabbit@gmail.com>
2025-05-12 01:47:57 +0900

    [S12P31A103-888] feature: Implement center join request list component

--- FE/src/pages/manager/CenterManagerMainPage.tsx
+++ FE/src/pages/manager/CenterManagerMainPage.tsx
@@ -1,17 +1,11 @@
+import AddressBookPanel from "@/components/manager/center/AddressBookPanel";
+import RequestPanel from "@/components/manager/center/RequestPanel";
+
 export default function CenterManagerMainPage() {
 	return (
 		<div className="mx-2.5 gap-4.5 flex flex-col">
-			<div className="flex flex-col bg-white rounded-lg shadow-custom-sm p-4 text-grayText font-semibold">
-				<div className="flex w-full justify-between items-center">
-					<span>지역 추가</span>
-					<button
-						type="button"
-						className="bg-male text-white rounded-full px-4 py-1 font-semibold"
-					>
-						추가
-					</button>
-				</div>
-			</div>
+			<AddressBookPanel />
+			<RequestPanel />
 		</div>
 	);
 }
