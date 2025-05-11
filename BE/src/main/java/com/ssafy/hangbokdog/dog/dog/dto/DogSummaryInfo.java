2b47e07f1f5a38715a5e84ed49603ba78ff7b665
freeftr <jonghaparkdevelop@gmail.com>
2025-05-11 14:16:38 +0900

    [S12P31A103-875] refactor: Fix dog-pagenation

--- BE/src/main/java/com/ssafy/hangbokdog/dog/dog/dto/DogSummaryInfo.java
+++ BE/src/main/java/com/ssafy/hangbokdog/dog/dog/dto/DogSummaryInfo.java
@@ -1,8 +1,8 @@
 package com.ssafy.hangbokdog.dog.dog.dto;
 
-import com.ssafy.hangbokdog.dog.dog.domain.enums.Gender;
-
 import java.time.LocalDateTime;
+
+import com.ssafy.hangbokdog.dog.dog.domain.enums.Gender;
 
 public record DogSummaryInfo(
 	Long dogId,
