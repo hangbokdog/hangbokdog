142a0810a20d91753baa5d9e4ae0a6b701e752f7
최준혁 <raonrabbit@gmail.com>
2025-05-13 18:11:50 +0900

    [S12P31A103-977] feature: Add scrolling to dog info item component

--- FE/src/components/dog/DogInfoItem.tsx
+++ FE/src/components/dog/DogInfoItem.tsx
@@ -14,7 +14,7 @@ export default function DogInfoItem({
 	return (
 		<div className={`flex  ${className}`}>
 			<span className="flex-1/5 font-medium py-2 px-2 w-20">{label}</span>
-			<span className="flex-4/5 text-grayText font-semibold bg-background py-2 px-5 rounded-[8px]">
+			<span className="flex-4/5 text-grayText overflow-y-auto font-semibold bg-background py-2 px-5 rounded-[8px] break-words max-h-[500px]">
 				{value}
 			</span>
 		</div>
