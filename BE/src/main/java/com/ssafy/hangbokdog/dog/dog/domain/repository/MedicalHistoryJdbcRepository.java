82db817cbeef29215210b467d3216a4bfb7968d3
박종하 <jonghaparkdevelop@gmail.com>
2025-05-14 15:42:37 +0900

    [S12P31A103-1048] refactor: Check Duplicate

--- BE/src/main/java/com/ssafy/hangbokdog/dog/dog/domain/repository/MedicalHistoryJdbcRepository.java
+++ BE/src/main/java/com/ssafy/hangbokdog/dog/dog/domain/repository/MedicalHistoryJdbcRepository.java
@@ -27,7 +27,7 @@ public class MedicalHistoryJdbcRepository {
 	) {
 		String sql = "INSERT INTO medical_history"
 			+ "(dog_id, content, medical_history_image, "
-			+ "medical_period, medical_type, operated_time, created_at, modified_at)"
+			+ "medical_period, medical_type, operated_date, created_at, modified_at)"
 			+ " VALUES (?, ?, ?, ?, ?, ?, NOW(), NOW())";
 
 		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
