2413e48a1357d703bf7519b02cb0cefc708dcbf0
raonrabbit <raonrabbit@gmail.com>
2025-05-16 06:21:46 +0900

    [S12P31A103-1134] feature: Implement adoption API

--- FE/src/api/adoption.ts
+++ FE/src/api/adoption.ts
@@ -1,4 +1,6 @@
-import localAxios from "./http-commons";
+import type { DogBreed, DogStatus, Gender } from "@/types/dog";
+import localAxios, { type PageInfo } from "./http-commons";
+import type { AdoptionStatus } from "@/types/adoption";
 
 export interface AdoptionApplicationResponse {
 	dogId: number;
@@ -42,23 +44,100 @@ export const fetchAdoptionApplicationsByDogAPI = async (
 	return response.data;
 };
 
-export interface AdoptionStatus {
-	APPLIED: "APPLIED";
-	UNDER_REVIEW: "UNDER_REVIEW";
-	REJECTED: "REJECTED";
-	ACCEPTED: "ACCEPTED";
-}
-
 export const manageAdoptionApplicationAPI = async (
 	adoptionId: number,
 	request: AdoptionStatus,
 	centerId: number,
 ) => {
-	const response = await localAxios.patch(`/adoptions/${adoptionId}`, {
+	const response = await localAxios.patch(`/adoptions/${adoptionId}`, null, {
 		params: {
 			request,
 			centerId,
 		},
+	});
+	return response.data;
+};
+
+export interface AdoptedDogDetailsResponse {
+	dogId: number;
+	dogStatus: DogStatus;
+	centerId: number;
+	centerName: string;
+	dogName: string;
+	profileImageUrl: string;
+	color: string[];
+	rescuedDate: string;
+	weight: number;
+	description: string;
+	isStar: boolean;
+	gender: Gender;
+	isNeutered: boolean;
+	breed: DogBreed;
+	age: number;
+	isLiked: boolean;
+	favoriteCount: number;
+	dogCommentCount: number;
+	adoptedDate: string;
+	adopterId: number;
+	adopterName: string;
+	adopterImage: string;
+}
+
+export const fetchAdoptedDogDetailsAPI = async (
+	dogId: number,
+): Promise<AdoptedDogDetailsResponse> => {
+	const response = await localAxios.get(`/adoptions/${dogId}`);
+	return response.data;
+};
+
+export const fetchAppliesCountOfDogs = async (centerId: number) => {
+	const response = await localAxios.get("/adoptions/appliesCount", {
+		params: {
+			centerId,
+		},
+	});
+	return response.data;
+};
+
+export const fetchAdoptedDogCount = async (centerId: number) => {
+	const response = await localAxios.get("/adoptions/adoptedCount", {
+		params: {
+			centerId,
+		},
+	});
+	return response.data;
+};
+
+export interface AdoptionSearchRequest {
+	centerId: number;
+	name?: string;
+	breed?: DogBreed[];
+	gender?: Gender;
+	start?: string;
+	end?: string;
+	isNeutered?: boolean;
+	isStar?: boolean;
+	pageToken?: string;
+}
+
+export interface AdoptionSearchResponse {
+	dogId: number;
+	name: string;
+	imageUrl: string;
+	ageMonth: number;
+	breed: DogBreed;
+	gender: Gender;
+	isNeutered: boolean;
+	isStar: boolean;
+	adoptedDate: string;
+	isFavorite: boolean;
+}
+
+export const adoptionSearchAPI = async (
+	params: AdoptionSearchRequest,
+): Promise<PageInfo<AdoptionSearchResponse>> => {
+	const response = await localAxios.get("/adoptions/search", {
+		params,
 	});
 	return response.data;
 };
