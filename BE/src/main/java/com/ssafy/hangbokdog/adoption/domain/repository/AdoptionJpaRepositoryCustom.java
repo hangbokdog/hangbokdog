a4277152b44880b1743264f8e7a3e844130e1eff
박종하 <jonghaparkdevelop@gmail.com>
2025-05-15 13:54:04 +0900

    [S12P31A103-1094] refactor: Change api spec

--- BE/src/main/java/com/ssafy/hangbokdog/adoption/domain/repository/AdoptionJpaRepositoryCustom.java
+++ BE/src/main/java/com/ssafy/hangbokdog/adoption/domain/repository/AdoptionJpaRepositoryCustom.java
@@ -1,15 +1,32 @@
 package com.ssafy.hangbokdog.adoption.domain.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.ssafy.hangbokdog.adoption.dto.AdoptedDogDetailInfo;
+import com.ssafy.hangbokdog.adoption.dto.AdoptionSearchInfo;
 import com.ssafy.hangbokdog.adoption.dto.response.AdoptionApplicationByDogResponse;
 import com.ssafy.hangbokdog.adoption.dto.response.AdoptionApplicationResponse;
+import com.ssafy.hangbokdog.dog.dog.domain.enums.DogBreed;
+import com.ssafy.hangbokdog.dog.dog.domain.enums.Gender;
 
 public interface AdoptionJpaRepositoryCustom {
 	List<AdoptionApplicationResponse> getAdoptionApplicationsByCenterId(Long centerId);
 
 	AdoptedDogDetailInfo getAdoptedDogDetail(Long dogId);
 
-	List<AdoptionApplicationByDogResponse> getAdoptionApplicationsByDogId(Long dogId);
+	List<AdoptionApplicationByDogResponse> getAdoptionApplicationsByDogId(Long dogId, String name);
+
+	List<AdoptionSearchInfo> search(
+		String name,
+		Long centerId,
+		List<DogBreed> breeds,
+		Gender gender,
+		LocalDateTime start,
+		LocalDateTime end,
+		Boolean isNeutered,
+		Boolean isStar,
+		String pageToken,
+		int pageSize
+	);
 }
