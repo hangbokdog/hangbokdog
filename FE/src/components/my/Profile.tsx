92507b40f1640fbd7e4e3ce90de4c22e8933905d
kimminji <neul252@gmail.com>
2025-05-15 14:38:05 +0900

    [S12P31A103-1096] style: Change profile edit icon

--- FE/src/components/my/Profile.tsx
+++ FE/src/components/my/Profile.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUserInfoAPI } from "@/api/auth";
 import { ProfileInfo } from "./ProfileInfo";
-import { FaPencilAlt } from "react-icons/fa";
+import { LuPencil } from "react-icons/lu";
 
 interface ProfileProps {
 	onEdit?: () => void;
@@ -26,7 +26,7 @@ export default function Profile({ onEdit }: ProfileProps) {
 				/>
 			</div>
 			{onEdit && (
-				<FaPencilAlt
+				<LuPencil
 					className="h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700"
 					onClick={onEdit}
 					aria-label="프로필 편집"
