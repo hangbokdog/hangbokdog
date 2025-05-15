9496985f90a0b88a185dd7767b1aca0fb552db3c
최준혁 <raonrabbit@gmail.com>
2025-05-15 17:25:56 +0900

    [S12P31A103-1104] feature: Implement adoption API

--- FE/src/api/adoption.ts
+++ FE/src/api/adoption.ts
@@ -1,24 +1,63 @@
-import type { PageInfo } from "./http-commons";
 import localAxios from "./http-commons";
 
 export interface AdoptionApplicationResponse {
-	adoptionId: number;
 	dogId: number;
 	dogName: string;
 	dogImage: string;
-	memberId: number;
-	memberName: string;
-	created_at: string;
+	count: number;
+	createdAt: string;
 }
 
 export const fetchAdoptionApplicationsAPI = async (
 	centerId: number,
-	pageToken?: string,
-): Promise<PageInfo<AdoptionApplicationResponse>> => {
+): Promise<AdoptionApplicationResponse[]> => {
 	const response = await localAxios.get("/adoptions", {
 		params: {
 			centerId,
-			pageToken,
+		},
+	});
+	return response.data;
+};
+
+export interface AdoptionApplicationByDogResponse {
+	memberId: number;
+	name: string;
+	profileImage: string;
+	phoneNumber: string;
+	createdAt: string;
+}
+
+export const fetchAdoptionApplicationsByDogAPI = async (
+	dogId: number,
+	centerId: number,
+	name?: string,
+): Promise<AdoptionApplicationByDogResponse[]> => {
+	const response = await localAxios.get(`/adoptions/${dogId}/applications`, {
+		params: {
+			centerId,
+			name,
+		},
+	});
+
+	return response.data;
+};
+
+export interface AdoptionStatus {
+	APPLIED: "APPLIED";
+	UNDER_REVIEW: "UNDER_REVIEW";
+	REJECTED: "REJECTED";
+	ACCEPTED: "ACCEPTED";
+}
+
+export const manageAdoptionApplicationAPI = async (
+	adoptionId: number,
+	request: AdoptionStatus,
+	centerId: number,
+) => {
+	const response = await localAxios.patch(`/adoptions/${adoptionId}`, {
+		params: {
+			request,
+			centerId,
 		},
 	});
 	return response.data;
