cc0312f95e3b3057ae24c8fdd3cff6ad8bbfb83d
freeftr <jonghaparkdevelop@gmail.com>
2025-05-17 16:56:29 +0900

    [S12P31A103-1186] feature: Implement CenterMember Notification

---
+++ BE/src/main/java/com/ssafy/hangbokdog/fcm/application/CenterMemberEventHandler.java
@@ -0,0 +1,24 @@
+package com.ssafy.hangbokdog.fcm.application;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.ssafy.hangbokdog.fcm.dto.event.CenterMemberEvent;
+import com.ssafy.hangbokdog.member.domain.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CenterMemberEventHandler {
+
+	private final FcmService fcmService;
+	private final MemberRepository memberRepository;
+
+	@EventListener
+	public void handleCenterMemberEvent(CenterMemberEvent event) {
+		String targetToken = memberRepository.getFcmTokenByMemberId(event.memberId());
+
+		fcmService.sendMessageTo(targetToken, event.centerName(), event.isApproved().toString());
+	}
+}
