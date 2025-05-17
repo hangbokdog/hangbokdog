1683aafeb83e2e54cfd86bb477668c5fac23be57
dbswlrla112@naver.com <dbswlrla112@naver.com>
2025-05-17 16:44:54 +0900

    [S12P31A103-1185] feat: Enable scroll-to-top behavior after routing

--- FE/src/layouts/MainLayout.tsx
+++ FE/src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
+import ScrollToTop from "@/components/common/ScrollToTop";
 import { Toaster } from "@/components/ui/sonner";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
 	return (
 		<div className="w-screen min-h-screen-safe h-auto flex justify-center">
+			<ScrollToTop />
 			<div className="flex flex-col w-full max-w-[440px] bg-background shadow-custom-xs">
 				<Outlet />
 			</div>
