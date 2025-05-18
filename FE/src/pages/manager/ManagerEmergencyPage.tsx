490c8b92ead153172637ee830df8b89550e063a0
kimminji <neul252@gmail.com>
2025-05-19 01:10:07 +0900

    [S12P31A103-1211] fix: Delete unused components

--- FE/src/pages/manager/ManagerEmergencyPage.tsx
+++ FE/src/pages/manager/ManagerEmergencyPage.tsx
@@ -1,10 +1,8 @@
 import EmergencyPanel from "@/components/manager/emergency/EmergencyPanel";
-import FlightVolunteer from "@/components/manager/emergency/FlightVolunteer";
 
 export default function ManagerEmergencyPage() {
 	return (
 		<div>
-			<FlightVolunteer />
 			<EmergencyPanel />
 		</div>
 	);
