cf6467a3dd4a203106c0daae502a2da19b5c2080
박종하 <jonghaparkdevelop@gmail.com>
2025-05-19 10:13:51 +0900

    [S12P31A103-1231] refactor: Add CommentLike Infos to CommentResponse

--- BE/src/main/java/com/ssafy/hangbokdog/post/comment/domain/repository/CommentRepository.java
+++ BE/src/main/java/com/ssafy/hangbokdog/post/comment/domain/repository/CommentRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.hangbokdog.post.comment.domain.Comment;
+import com.ssafy.hangbokdog.post.comment.dto.CommentInfo;
+import com.ssafy.hangbokdog.post.comment.dto.CommentLikeInfo;
 import com.ssafy.hangbokdog.post.comment.dto.response.CommentResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class CommentRepository {
 
     private final CommentJpaRepository commentJpaRepository;
+    private final CommentLikeJpaRepository commentLikeJpaRepository;
 
     public Comment save(Comment comment) {
         return commentJpaRepository.save(comment);
@@ -28,7 +31,15 @@ public class CommentRepository {
         return commentJpaRepository.findByCommentId(commentId, loginId);
     }
 
-    public List<CommentResponse> findAllByPostId(Long postId, Long loginId) {
+    public List<CommentInfo> findAllByPostId(Long postId, Long loginId) {
         return commentJpaRepository.findAllByPostId(postId, loginId);
+    }
+
+    public List<CommentLikeInfo> findCommentLikeByPostId(Long postId) {
+        return commentLikeJpaRepository.findCommentLikeByPostId(postId);
+    }
+
+    public List<Long> getCommentLikeIdsByMemberId(Long memberId) {
+        return commentLikeJpaRepository.findByMemberId(memberId);
     }
 }
