6cfcbc4f53827f6420e2c7c91d719d2cda47066a
jinsu868 <mjinsu098@gmail.com>
2025-05-19 14:20:57 +0900

    [S12P31A103-891] refactor : add center approve comment

--- BE/src/main/java/com/ssafy/hangbokdog/fcm/dto/response/CenterNotification.java
+++ BE/src/main/java/com/ssafy/hangbokdog/fcm/dto/response/CenterNotification.java
@@ -5,11 +5,12 @@ import java.time.LocalDateTime;
 import com.ssafy.hangbokdog.fcm.domain.NotificationType;
 
 public record CenterNotification(
+        String content,
         boolean isApproved,
         LocalDateTime createdAt,
         NotificationType type
 ) {
-    public static CenterNotification from(boolean isApproved) {
-        return new CenterNotification(isApproved, LocalDateTime.now(), NotificationType.CENTER);
+    public static CenterNotification of(String content, boolean isApproved) {
+        return new CenterNotification(content, isApproved, LocalDateTime.now(), NotificationType.CENTER);
     }
 }
