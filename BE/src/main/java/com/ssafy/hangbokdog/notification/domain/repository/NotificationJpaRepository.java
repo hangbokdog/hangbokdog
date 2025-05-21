eb1e025508b07dc2db8976c82479131fd3722cbe
jinsu868 <mjinsu098@gmail.com>
2025-05-21 15:08:00 +0900

    [S12P31A103-891] fix : typo

--- BE/src/main/java/com/ssafy/hangbokdog/notification/domain/repository/NotificationJpaRepository.java
+++ BE/src/main/java/com/ssafy/hangbokdog/notification/domain/repository/NotificationJpaRepository.java
@@ -1,12 +1,14 @@
 package com.ssafy.hangbokdog.notification.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ssafy.hangbokdog.notification.domain.Notification;
 
 public interface NotificationJpaRepository extends JpaRepository<Notification, Long>, NotificationJpaRepositoryCustom {
 
+    @Modifying
     @Query("DELETE FROM Notification n WHERE n.receiverId = :receiverId")
     void deleteAllByReceiverId(Long receiverId);
 }
